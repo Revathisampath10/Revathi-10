7b3ed5abe51a49bdf832090024a0974830bf4c85
Revathisampath10 <revasampath107@gmail.com>
2023-05-31 10:39:30 +0530

    new project creation

---
+++ src/main/java/SeleniumPractice/SeleniumPractice/EvenNumber.java
@@ -0,0 +1,17 @@
+package SeleniumPractice.SeleniumPractice;
+
+public class EvenNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number =100;
+		for(int i=1;i<=number;i++) {
+			{
+				if(i%3!=0) {
+					System.out.print(i+" ");
+				}
+			}
+	}
+	}
+}
+
